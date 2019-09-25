d12cf6aae8266f996943cb3df9024edbce88a41e
Jacqueline Remmel and Kira Roberts <kira@>
2019-09-25 14:27:44 -0700

    Add Queen class with constructor & getters.

--- queen.cs
+++ queen.cs
@@ -1,2 +1,39 @@
 using System;
 
+namespace Chess
+{
+  class Queen
+  {
+    private int _row;
+    private int _col;
+    public Queen(int row, int col)
+    {
+      _row = row;
+      _col = col;
+    }
+
+    public int GetRow()
+    {
+      return _row;
+    }
+
+    public int GetCol()
+    {
+      return _col;
+    }
+
+    public bool CanAttack(int x, int y)
+    {
+      bool canAttack = false;
+
+
+
+
+      return canAttack;
+    }
+
+
+  }
+
+  
+}
